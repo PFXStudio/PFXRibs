d0d91701aea4d5f45260e62b4ccfd40bba6198f6
pfxstudio21 <pfxstudio21@gmail.com>
2020-02-09 22:12:15 +0900

    add proj start ribs architecture

---
+++ PFXRibs/PFXRibs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PFXRibs
+//
+//  Created by succorer on 09/02/2020.
+//  Copyright © 2020 PFXStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
